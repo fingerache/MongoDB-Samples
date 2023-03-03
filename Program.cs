301a446ef9f43b3ddb65342e141e36343a7f98b9
srinath <dis4code@gmail.com>
2023-03-03 06:06:12 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+
+namespace MDB_Changestream
+{
+    public static class MongoClientFactory
+    { 
+        private static MongoClient _client;
+        private static Object lockobj;
+        private static string connString = "mongodb+srv://MNG1:MNG001@mission24.s2ml3.mongodb.net/"
+                                        +"?retryWrites=true&w=majority";
+        public static MongoClient getMongoClient()
+        {
+            if (_client == null)
+            {
+                lockobj = new Object();
+                lock (lockobj)
+                {
+                    if (_client == null)
+                    {
+                        _client = new MongoClient(connString);
+                    }
+                }
+            }
+            return _client;
+        }
+    }
+
+    
+
+
+    internal class Program
+    {
+        static PipelineDefinition<ChangeStreamDocument<BsonDocument>,
+            ChangeStreamDocument<BsonDocument>> getInsertUpdateFilter()
+        {
+            var pipeline = new EmptyPipelineDefinition<ChangeStreamDocument<BsonDocument>>()
+                    .Match(x => x.OperationType == ChangeStreamOperationType.Insert || 
+                        x.OperationType == ChangeStreamOperationType.Update);
+
+            return pipeline;
+        }   
+
+        static void Main(string[] args)
+        {
+            var dbclient = MongoClientFactory.getMongoClient();
+            var database = dbclient.GetDatabase("test");
+
+
+            //using (var cursor = database.Watch())
+                // OR use the following for more filtering
+            using (var cursor = database.Watch(getInsertUpdateFilter()))
+            {
+                foreach(var change in cursor.ToEnumerable())
+                {
+                    Console.WriteLine(change.BackingDocument.ToString());
+                }
+            }
+
+            Console.WriteLine("Exiting...");
+            Console.ReadLine();
+        }
+    }
+}
